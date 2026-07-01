375f20c7a417c9a47bfd1f9b8a1fc43cf7978589
amkocar <paidvertssistem@gmail.com>
2026-07-01 21:55:14 +0200

    Update debug.js

--- api/auth/debug.js
+++ api/auth/debug.js
@@ -1,1 +1,13 @@
-
+module.exports = async (req, res) => {
+  res.status(200).json({
+    ok: true,
+    env: {
+      WHOP_PRODUCT_ID: Boolean(process.env.WHOP_PRODUCT_ID),
+      WHOP_CLIENT_ID: Boolean(process.env.WHOP_CLIENT_ID),
+      WHOP_CLIENT_SECRET: Boolean(process.env.WHOP_CLIENT_SECRET),
+      WHOP_API_KEY: Boolean(process.env.WHOP_API_KEY),
+      WHOP_SESSION_SECRET: Boolean(process.env.WHOP_SESSION_SECRET),
+      WHOP_REDIRECT_URI: Boolean(process.env.WHOP_REDIRECT_URI),
+    },
+  });
+};
