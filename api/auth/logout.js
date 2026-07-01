a391e63da7d09d87404fa428277a17215f52a9ea
amkocar <paidvertssistem@gmail.com>
2026-07-01 21:56:06 +0200

    Update logout.js

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -1,1 +1,5 @@
-
+module.exports = async (req, res) => {
+  res.setHeader("Set-Cookie", "maa_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0");
+  res.writeHead(302, { Location: "/paywall.html?logged_out=1" });
+  res.end();
+};
